75cfc5cf1584943007626aaf7767be2e9ced0a8e
Alan R. Rogers <rogers@anthro.utah.edu>
2015-07-16 16:42:19 -0600

    Roughed in parser.

--- src/mktree.c
+++ src/mktree.c
@@ -1,15 +1,27 @@
 #include "gptree.h"
+#include "tokenizer.h"
 
 // One line of input for each segment of population tree.
 // Each line is of form
 //
-//   x < 0.8 y < 0.2 z 2N=1e5 t=1000 samples=2
+//   x < 0.8 y < z 2N=1e5 t=1000 samples=2
 //
 // which says that population x got 80% of its genes from y and 20%
 // from z, has size 2N=1e5, duration 1000 generations, and that we
-// sampled 2 haploid genomes.
+// sampled 2 haploid genomes. If there was only a single parental
+// population, the input line would look like this
+//
+//   x < y 2N=1e5 t=1000 samples=2
+
+#define CHECK_INDEX(ndx,n) do{                                          \
+    if((ndx)>=(n)){                                                     \
+    fprintf(stderr,"%s:%s:%d: index out of bounds\n",                   \
+            __FILE__,__func__,__LINE__);                                \
+    exit(EXIT_FAILURE);                                                 \
+    }while(0)
 
 int getDbl(double *x, Tokenizer *tkz, int i);
+int getULong(unsigned long *x, Tokenizer *tkz, int i);
 
 int getDbl(double *x, Tokenizer *tkz, int i) {
     char *end=NULL;
@@ -20,6 +32,16 @@ int getDbl(double *x, Tokenizer *tkz, int i) {
     return 1;      // failure
 }
 
+int getULong(unsigned long *x, Tokenizer *tkz, int i) {
+    char *end=NULL;
+
+    *x = strtoul(Tokenizer_token(tkz, i), &end, 10);
+    if(end!=NULL && *end == '\0')
+        return 0;  // success
+    return 1;      // failure
+}
+
+
 PopNode *mktree(FILE *fp, HashTab *ht) {
     int ntokens;
     char buff[500];
@@ -27,12 +49,13 @@ PopNode *mktree(FILE *fp, HashTab *ht) {
 
     char *name0, *name1, *name2, *s;
     El *el0, *el1, *el2;
-    double p1, p2;
-    int got_p1, got_p2, curr, nsamples;
+    double p, twoN, t;
+    unsigned samples;
+    int got_p, curr, nsamples;
 
     while(1) {
         curr = 0;
-        got_p1 = got_p2 = nsamples = 0;
+        got_p = nsamples = 0;
         name0 = name1 = name2 = NULL;
         el0 = el1 = el2 = NULL;
         if(fgets(buff, sizeof buff, fp) == NULL)
@@ -44,48 +67,39 @@ PopNode *mktree(FILE *fp, HashTab *ht) {
                     __FILE__, __LINE__, sizeof(buff));
         Tokenizer_split(tkz, buff, " \t="); // tokenize
         ntokens = Tokenizer_strip(tkz, " \t=\n");
+        CHECK_INDEX(curr, ntokens);
         name0 = Tokenizer_token(tkz, curr++);
+        CHECK_INDEX(curr, ntokens);
         if('<' != *Tokenizer_token(tkz, curr++)) {
             fprintf(stderr,"Bad input line:\n");
             Tokenizer_print(tkz, stderr);
             exit(EXIT_FAILURE);
         }
-        if(!getDbl(&p1, tkz, curr)) {
-            got_p1 = 1;
-            if(p1 <= 0 || p1 >= 1.0) {
+        CHECK_INDEX(curr, ntokens);
+        if(!getDbl(&p, tkz, curr)) {
+            got_p = 1;
+            if(p <= 0 || p >= 1.0) {
                 fprintf(stderr,"Bad probability, %lf, in input line:\n", p1);
                 Tokenizer_print(tkz, stderr);
                 exit(EXIT_FAILURE);
             }
             ++curr;
         }
+        CHECK_INDEX(curr, ntokens);
         name1 = Tokenizer_token(tkz, curr++);
-        if('<' != *Tokenizer_token(tkz, curr++)) {
-            fprintf(stderr,"Bad input line:\n");
-            Tokenizer_print(tkz, stderr);
-            exit(EXIT_FAILURE);
-        }
-        if(!getDbl(&p2, tkz, curr)) {
-            got_p2 = 1;
-            if(p2 <= 0 || p2 >= 1.0) {
-                fprintf(stderr,"Bad probability, p2=%lf, in input line:\n", p1);
-                Tokenizer_print(tkz, stderr);
-                exit(EXIT_FAILURE);
-            }
-            if(fabs(1.0 - p1 - p2) > 0.02) {
-                fprintf(stderr,"%lf + %ld != 1.0\n", p1, p2);
-                Tokenizer_print(tkz, stderr);
-                exit(EXIT_FAILURE);
-            }
+        CHECK_INDEX(curr, ntokens);
+        if('<' == *Tokenizer_token(tkz, curr)) {
+            name2 = Tokenizer_token(tkz, ++curr);
             ++curr;
         }
-        name2 = Tokenizer_token(tkz, curr++);
+        CHECK_INDEX(curr, ntokens);
         if(0 != strcmp("2N", Tokenizer_token(tkz, curr++))) {
             fprintf(stderr,"Got %s when expecting 2N on input:\n",
                     Tokenizer_token(tkz, curr-1));
             Tokenizer_print(tkz, stderr);
             exit(EXIT_FAILURE);
         }
+        CHECK_INDEX(curr, ntokens);
         if(!getDbl(&twoN, tkz, curr)) {
             if(twoN <= 0.0) {
                 fprintf(stderr,"Bad value 2N=%lf, in input line:\n", twoN);
@@ -93,6 +107,61 @@ PopNode *mktree(FILE *fp, HashTab *ht) {
                 exit(EXIT_FAILURE);
             }
             ++curr;
+        }else{
+            fprintf(stderr,"Can't parse \"%s\" as a double. Expecting value of 2N\n",
+                    Tokenizer_token(tkz, curr));
+            Tokenizer_print(tkz, stderr);
+            exit(EXIT_FAILURE);
+        }
+        CHECK_INDEX(curr, ntokens);
+        if(0 != strcmp("t", Tokenizer_token(tkz, curr++))) {
+            fprintf(stderr,"Got %s when expecting t on input:\n",
+                    Tokenizer_token(tkz, curr-1));
+            Tokenizer_print(tkz, stderr);
+            exit(EXIT_FAILURE);
+        }
+        CHECK_INDEX(curr, ntokens);
+        if(!getDbl(&t, tkz, curr)) {
+            if(t <= 0.0) {
+                fprintf(stderr,"Bad value t=%lf, in input line:\n", t);
+                Tokenizer_print(tkz, stderr);
+                exit(EXIT_FAILURE);
+            }
+            ++curr;
+        }else{
+            fprintf(stderr,"Can't parse \"%s\" as a double. Expecting value of t\n",
+                    Tokenizer_token(tkz, curr));
+            Tokenizer_print(tkz, stderr);
+            exit(EXIT_FAILURE);
+        }
+        if(curr == ntokens)
+            continue;
+        CHECK_INDEX(curr, ntokens);
+        if(0 != strcmp("samples", Tokenizer_token(tkz, curr++))) {
+            fprintf(stderr,"Got %s when expecting \"samples\" on input:\n",
+                    Tokenizer_token(tkz, curr-1));
+            Tokenizer_print(tkz, stderr);
+            exit(EXIT_FAILURE);
+        }
+        CHECK_INDEX(curr, ntokens);
+        if(!getULong(&samples, tkz, curr)) {
+            if(samples > 1000) {
+                fprintf(stderr,"Bad value samples=%u, in input line:\n", samples);
+                Tokenizer_print(tkz, stderr);
+                exit(EXIT_FAILURE);
+            }
+            ++curr;
+        }else{
+            fprintf(stderr,"Can't parse \"%s\" as an unsigned int."
+                    " Expecting value of \"samples\"\n",
+                    Tokenizer_token(tkz, curr));
+            Tokenizer_print(tkz, stderr);
+            exit(EXIT_FAILURE);
+        }
+        if(curr < ntokens) {
+            fprintf(stderr,"Too many tokens on input line:\n");
+            Tokenizer_print(tkz, stderr);
+            exit(EXIT_FAILURE);
         }
     }
 
