73d28ca99bd58ab554af351d675a4c2208b5b865
Alan R. Rogers <rogers@anthro.utah.edu>
2016-05-15 08:03:07 -0600

    Ran indent on cost.c

--- src/cost.c
+++ src/cost.c
@@ -13,7 +13,7 @@ Systems Consortium License, which can be found in file "LICENSE".
 
 #include "dprintf.h"
 #ifdef DPRINTF_ON
-#  include <pthread.h>
+#include <pthread.h>
 extern pthread_mutex_t outputLock;
 #endif
 
@@ -22,11 +22,11 @@ extern pthread_mutex_t outputLock;
 /// @param[in] x vector of parameter values.
 double costFun(int dim, double x[dim], void *jdata, void *tdata) {
     const CostPar *cp = (CostPar *) jdata;
-	gsl_rng *rng = (gsl_rng *) tdata;
+    gsl_rng    *rng = (gsl_rng *) tdata;
 
     GPTree_setParams(cp->gptree, dim, x);
-    BranchTab *prob = patprob(cp->gptree, cp->nThreads, cp->nreps,
-                              cp->pointNdx, rng);
+    BranchTab  *prob = patprob(cp->gptree, cp->nThreads, cp->nreps,
+                               cp->pointNdx, rng);
     BranchTab_normalize(prob);
     return BranchTab_KLdiverg(prob, cp->obs);
 }
