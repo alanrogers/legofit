b4edf5585464ceaab4f3f2d45e2bbe1a181e5094
Alan R. Rogers <rogers@anthro.utah.edu>
2016-09-05 13:25:50 -0600

    Use wider field in printing parameter names: parstore.c

--- src/parstore.c
+++ src/parstore.c
@@ -68,7 +68,7 @@ void ParStore_print(ParStore *self, FILE *fp) {
     int i;
     fprintf(fp, "%5d fixed:\n", self->nFixed);
     for(i=0; i < self->nFixed; ++i)
-        fprintf(fp, "   %6s = %lf\n", self->nameFixed[i], self->fixedVal[i]);
+        fprintf(fp, "   %8s = %lf\n", self->nameFixed[i], self->fixedVal[i]);
     ParStore_printFree(self, fp);
 }
 
@@ -76,7 +76,7 @@ void ParStore_printFree(ParStore *self, FILE *fp) {
     int i;
     fprintf(fp, "%5d free :\n", self->nFree);
     for(i=0; i < self->nFree; ++i)
-        fprintf(fp, "   %6s = %lf\n", self->nameFree[i], self->freeVal[i]);
+        fprintf(fp, "   %8s = %lf\n", self->nameFree[i], self->freeVal[i]);
 }
 
 /// Constructor
