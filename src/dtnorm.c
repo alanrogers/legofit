ae139aec54cd1ba62cf7fb1a233e48bc5d79a88b
Alan R. Rogers <rogers@anthro.utah.edu>
2016-12-21 11:53:49 -0700

    Update dtnorm.h and dtnorm.c

--- src/dtnorm.c
+++ src/dtnorm.c
@@ -1,20 +1,20 @@
 //  Pseudorandom numbers from a truncated Gaussian distribution.
 //
 // This code was translated by Alan R. Rogers into C, based on the C++
-// original written by G. Dolle and V Mazet, which is available at 
+// original written by G. Dolle and V Mazet, which is available at
 // http://miv.u-strasbg.fr/mazet/rtnorm/rtnormCpp.zip
 //
 //  This implements an extension of Chopin's algorithm detailed in
 //  N. Chopin, "Fast simulation of truncated Gaussian distributions",
 //  Stat Comput (2011) 21:275-288
-//  
+//
 //  Copyright (C) 2012 Guillaume Dollé, Vincent Mazet
 //  (LSIIT, CNRS/Université de Strasbourg)
 //  Version 2012-07-04, Contact: vincent.mazet@unistra.fr
-//  
+//
 //  06/07/2012:
 //  - first launch of rtnorm.cpp
-//  
+//
 //  Licence: GNU General Public License Version 2
 //  This program is free software; you can redistribute it and/or modify it
 //  under the terms of the GNU General Public License as published by the
@@ -25,7 +25,7 @@
 //  GNU General Public License for more details. You should have received a
 //  copy of the GNU General Public License along with this program; if not,
 //  see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
-//  
+//
 //  Depends: LibGSL
 //  OS: Unix based system
 
@@ -41,6 +41,12 @@
 #include "dtnorm_data.h"
 
 int         N = 4001;           // Index of the right tail
+
+// Compute y_l from y_k
+double yl(int k);
+
+// Rejection algorithm with a truncated exponential proposal
+double dtexp(gsl_rng *rng, double a, double b);
 
 // Pseudorandom numbers from a truncated Gaussian distribution
 // The Gaussian has parameters mu (default 0) and sigma (default 1)
@@ -79,11 +85,11 @@ double dtnorm(const double mu, const double sigma, double a, double b, gsl_rng *
     else if(fabs(a) > fabs(b))
         r = -dtnorm(0.0, 1.0, -b, -a, rng);
 
-    // Truncated exponential proposal   
+    // Truncated exponential proposal
     else if(a > 3.0 && bma > 0.5)
         r = dtexp(rng, a, b);
 
-    // Gaussian proposal 
+    // Gaussian proposal
     else if(a < 0.0 && b > 0.0 && bma > 1.0) {
         while(!stop) {
             r = gsl_ran_gaussian_ziggurat(rng, 1.0);
@@ -130,7 +136,7 @@ double dtnorm(const double mu, const double sigma, double a, double b, gsl_rng *
             );
 
         // If |b-a| is small, use rejection algorithm with a truncated
-        // exponential proposal 
+        // exponential proposal
         if(abs(kb - ka) < kmin) {
             r = dtexp(rng, a, b);
             stop = true;
@@ -176,7 +182,7 @@ double dtnorm(const double mu, const double sigma, double a, double b, gsl_rng *
                 simy = yu[k] * u;
                 d = x[k + 1] - x[k];
                 ylk = yl(k);
-                if(simy < ylk)  // That's what happens most of the time 
+                if(simy < ylk)  // That's what happens most of the time
                 {
                     r = x[k] + u * d * yu[k] / ylk;
                     stop = true;
--- src/dtnorm.h
+++ src/dtnorm.h
@@ -1,20 +1,20 @@
 //  Pseudorandom numbers from a truncated Gaussian distribution.
 //
 // This code was translated by Alan R. Rogers into C, based on the C++
-// original written by G. Dolle and V Mazet, which is available at 
+// original written by G. Dolle and V Mazet, which is available at
 // http://miv.u-strasbg.fr/mazet/rtnorm/rtnormCpp.zip
 //
 //  This implements an extension of Chopin's algorithm detailed in
 //  N. Chopin, "Fast simulation of truncated Gaussian distributions",
 //  Stat Comput (2011) 21:275-288
-//  
+//
 //  Copyright (C) 2012 Guillaume Dollé, Vincent Mazet
 //  (LSIIT, CNRS/Université de Strasbourg)
 //  Version 2012-07-04, Contact: vincent.mazet@unistra.fr
-//  
+//
 //  06/07/2012:
 //  - first launch of rtnorm.cpp
-//  
+//
 //  Licence: GNU General Public License Version 2
 //  This program is free software; you can redistribute it and/or modify it
 //  under the terms of the GNU General Public License as published by the
@@ -25,7 +25,7 @@
 //  GNU General Public License for more details. You should have received a
 //  copy of the GNU General Public License along with this program; if not,
 //  see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
-//  
+//
 //  Depends: LibGSL
 //  OS: Unix based system
 
@@ -35,15 +35,10 @@
 
 #include <gsl/gsl_rng.h>
 
-// Compute y_l from y_k
-double yl(int k);
-
-// Rejection algorithm with a truncated exponential proposal
-double dtexp(gsl_rng *rng, double a, double b);
-
 // Pseudorandom numbers from a truncated Gaussian distribution The
 // Gaussian has parameters mu (default 0) and sigma (default 1) and is
 // truncated on the interval [a,b].  Returns the random variate x.
-double dtnorm(const double mu, const double sigma, double a, double b, gsl_rng *rng);
+double dtnorm(const double mu, const double sigma, double a, double b,
+              gsl_rng *rng);
 
 #endif
