f60545dd12d57560d6c5f2325a87ad4b8a5cf6ca
Alan R. Rogers <rogers@anthro.utah.edu>
2017-01-15 12:54:11 -0700

    Using dtnorm instead of gsl_ran_gaussian to initialize twoN values within popnode.c.

--- src/popnode.c
+++ src/popnode.c
@@ -14,6 +14,7 @@
 #include "gene.h"
 #include "misc.h"
 #include "parstore.h"
+#include "dtnorm.h"
 #include <string.h>
 #include <float.h>
 #include <stdbool.h>
@@ -508,8 +509,13 @@ static void PopNode_randomize_r(PopNode *self, Bounds bnd, gsl_rng *rng) {
 
     // perturb self->twoN
     if(self->twoNfree) {
+        *self->twoN = dtnorm(*self->twoN, 10000.0, bnd.lo_twoN,
+                             bnd.hi_twoN, rng);
+#if 0
+        // replaced this code with above 2017-01-15
         *self->twoN += gsl_ran_gaussian(rng, 10000.0);
         *self->twoN = reflect(*self->twoN, bnd.lo_twoN, bnd.hi_twoN);
+#endif
     }
 
     // perturb self->start
