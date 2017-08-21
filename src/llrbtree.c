ce2f6a0586bc3503df0b714384308536c6442c7b
Alan R. Rogers <rogers@anthro.utah.edu>
2017-08-21 17:38:08 -0600

    First draft of llrbtree. Unit test compiles and runs, but tree isn't balanced.

--- src/llrbtree.c
+++ src/llrbtree.c
@@ -1,33 +1,48 @@
 // http://www.cs.princeton.edu/~rs/talks/LLRB/LLRB.pdf
+#include "llrbtree.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#define RED 1
+#define BLACK 0
+#define ISRED(node) ((node)!=NULL && (node)->color==RED)
 
-typedef enum Color Color;
-typedef struct Node Node;
-typedef tipId_t key_t;
-typedef double val_t;
-
-#define ISRED(node) ((node)!=NULL && (node)->color==red)
-
-enum Color {red, black};
-
-struct Node {
-    key_t key;
+struct BSTNode {
+    bstkey_t key;
     val_t value;
-    Color color;
-    Node *left, *right;
+    int color;
+    BSTNode *left, *right;
 };
 
-Node *Node_new(key_t key, val_t value) {
-    Node *self = malloc(sizeof(Node));
-    CHECKMEM(self);
+BSTNode *BSTNode_new(bstkey_t key, val_t value);
+BSTNode *BSTNode_insert_r(BSTNode *h, bstkey_t key, val_t value);
+BSTNode *BSTNode_rotateLeft(BSTNode *h);
+BSTNode *BSTNode_rotateRight(BSTNode *h);
+void BSTNode_flipColors(BSTNode *h);
+
+val_t BSTNode_value(BSTNode *h) {
+    return h->value;
+}
+
+bstkey_t BSTNode_key(BSTNode *h) {
+    return h->key;
+}
+
+BSTNode *BSTNode_new(bstkey_t key, val_t value) {
+    BSTNode *self = malloc(sizeof(BSTNode));
+    if(self==NULL) {
+        fprintf(stderr,"%s:%d: can't allocate memory\n",
+                __FILE__, __LINE__);
+        exit(1);
+    }
     self->key = key;
     self->value = value;
     return self;
 }
 
-Node *Node_search(Node *root, key_t key) {
-    Node *x = root;
+BSTNode *BSTNode_search(BSTNode *root, bstkey_t key) {
+    BSTNode *x = root;
     while(x != NULL) {
-        int cmp;
         if(key == x->key)
             return x;
         else if(key < x->key)
@@ -38,30 +53,54 @@ Node *Node_search(Node *root, key_t key) {
     return NULL;
 }
 
-Node *Node_insert(Node *root, key_t key, val_t value) {
-    root = Node_insert_r(root, key, value);
-    root->color = black;
+BSTNode *BSTNode_insert(BSTNode *root, bstkey_t key, val_t value) {
+    root = BSTNode_insert_r(root, key, value);
+    root->color = BLACK;
     return root;
 }
 
-Node *Node_insert_r(Node *h, key_t key, val_t value) {
+BSTNode *BSTNode_insert_r(BSTNode *h, bstkey_t key, val_t value) {
     if(h == NULL)
-        return Node_new(key, value);
+        return BSTNode_new(key, value);
     if(ISRED(h->left) && ISRED(h->right))
-        Node_colorFlip(h);
+        BSTNode_flipColors(h);
     if(key == h->key)
         h->value = value;
     else if( key < h->key )
-        h->left = Node_insert_r(h->left, key, value);
+        h->left = BSTNode_insert_r(h->left, key, value);
     else {
         assert(key > h->key);
-        h->right = Node_insert_r(h->right, key, value);
+        h->right = BSTNode_insert_r(h->right, key, value);
     }
-    if(ISRED(h->right) == red && !ISRED(h->left))
-        h = Node_rotateLeft(h);
+    if(ISRED(h->right) && !ISRED(h->left))
+        h = BSTNode_rotateLeft(h);
     if(ISRED(h->left) && ISRED(h->left->left))
-        h = Node_rotateRight(h);
+        h = BSTNode_rotateRight(h);
     return h;
+}
+
+BSTNode *BSTNode_rotateLeft(BSTNode *h) {
+    BSTNode *x = h->right;
+    h->right = x->left;
+    x->left = h;
+    x->color = h->color;
+    h->color = RED;
+    return x;
+}
+
+BSTNode *BSTNode_rotateRight(BSTNode *h) {
+    BSTNode *x = h->left;
+    h->left = x->right;
+    x->right = h;
+    x->color = h->color;
+    h->color = RED;
+    return x;
+}
+
+void BSTNode_flipColors(BSTNode *h) {
+    h->color = !h->color;
+    h->left->color = !h->left->color;
+    h->right->color = !h->right->color;
 }
 
 #if 0
@@ -112,6 +151,89 @@ public class LLRB<Key extends Comparable<Key>, Value>
         if (isRed(h.right) && !isRed(h.left)) h = rotateLeft(h);
         if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
         return h;
+    }
+
+    Node rotateLeft(Node h)
+    {
+        x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = h.color;
+        h.color = RED;
+        return x;
+    }
+    Node rotateRight(Node h)
+    {
+        x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = h.color;
+        h.color = RED;
+        return x;
+    }
+}
+#endif
+
+#ifdef TEST
+
+#  ifdef NDEBUG
+#    error "Unit tests must be compiled without -DNDEBUG flag"
+#  endif
+
+#include <string.h>
+
+void BSTNode_print(BSTNode *h, FILE *fp, int indent);
+
+void BSTNode_print(BSTNode *h, FILE *fp, int indent) {
+    if(h == NULL)
+        return;
+    BSTNode_print(h->left, fp, indent+1);
+    int i;
+    for(i=0; i<indent; ++i)
+        putc(' ', fp);
+    fprintf(fp, "[%u, %lf, %s]\n",
+            h->key,
+            h->value,
+            h->color==RED ? "red" : "black");
+    BSTNode_print(h->right, fp, indent+1);
+}
+
+int main(int argc, char **argv) {
+
+    int         verbose = 0;
+
+    if(argc > 1) {
+        if(argc != 2 || 0 != strcmp(argv[1], "-v")) {
+            fprintf(stderr, "usage: xllrbtree [-v]\n");
+            exit(EXIT_FAILURE);
+        }
+        verbose = 1;
+    }
+
+    BSTNode *root = NULL;
+    int i;
+    for(i=0; i < 100; ++i) {
+        bstkey_t key = (bstkey_t) i;
+        val_t value = (val_t) i;
+        root = BSTNode_insert(root, key, value);
+    }
+
+    if(verbose)
+        BSTNode_print(root, stdout, 0);
+
+    for(i=0; i < 100; ++i) {
+        bstkey_t key = (bstkey_t) i;
+        val_t value = (val_t) i;
+        BSTNode *found = BSTNode_search(root, key);
+        assert(found);
+        assert(BSTNode_key(found) == key);
+        assert(BSTNode_value(found) == value);
+    }
+
+    for(i=100; i < 110; ++i) {
+        bstkey_t key = (bstkey_t) i;
+        BSTNode *found = BSTNode_search(root, key);
+        assert(found == NULL);
     }
 }
 #endif
---
+++ src/llrbtree.h
@@ -0,0 +1,9 @@
+#include "typedefs.h"
+typedef struct BSTNode BSTNode;
+typedef tipId_t bstkey_t;
+typedef double val_t;
+
+val_t BSTNode_value(BSTNode *h);
+bstkey_t BSTNode_key(BSTNode *h);
+BSTNode *BSTNode_search(BSTNode *root, bstkey_t key);
+BSTNode *BSTNode_insert(BSTNode *root, bstkey_t key, val_t value);
