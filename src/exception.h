13b7d39f73ffab936e5d4d9324bfca6280f7be8b
Alan R. Rogers <rogers@anthro.utah.edu>
2017-11-03 16:16:10 -0600

    Add exception.h exception.c

---
+++ src/exception.h
@@ -0,0 +1,16 @@
+#ifndef EXCEPTION_INCLUDED
+#define EXCEPTION_INCLUDED
+
+#define EXCEPTION_SIZE 1000
+
+struct Exception {
+    struct Exception *next;
+    char buff[EXCEPTION_SIZE];
+};
+
+Exception *Exception_new(void);
+Exception *Exception_push(Exception *root, Exception *new);
+void Exception_abort(Exception *self);
+void Exception_free(Exception *self);
+
+#endif
