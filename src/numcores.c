4c101908b4b5110b04da32eed436d4b51cd351d0
Alan R. Rogers <rogers@anthro.utah.edu>
2017-11-15 15:53:16 -0700

    .

--- src/numcores.c
+++ src/numcores.c
@@ -1,6 +1,43 @@
 #include "misc.h"
 #include <stdio.h>
-int main(void) {
-    printf("%d\n", getNumCores());
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+void usage(void);
+
+void usage(void) {
+    fprintf(stderr,"usage: numcores [factor]\n");
+    fprintf(stderr,"  where \"factor\" is a number between 0 and 1.\n");
+    fprintf(stderr,"  Program prints factor times the number of cores,\n");
+    fprintf(stderr,"  rounded to nearest integer.\n");
+    fprintf(stderr,"  Default: factor=1.\n");
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char **argv) {
+    double factor;
+    char *end;
+    switch(argc) {
+    case 1:
+        printf("%d\n", getNumCores());
+        break;
+    case 2:
+        errno=0;
+        factor = strtod(argv[1], &end);
+        if(errno) {
+            fprintf(stderr,"Bad float: %s (%s)\n",
+                    argv[1], strerror(errno));
+            usage();
+        }else if(end == argv[1]) {
+            fprintf(stderr,"Bad float: %s\n",
+                    argv[1]);
+            usage();
+        }
+        printf("%0.0lf\n", floor(0.5 + factor * getNumCores()));
+        break;
+    default:
+        usage();
+        break;
+    }
     return 0;
 }
