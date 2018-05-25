b023c61728ae8bb313c090104e8934e554cb3c5d
Darokrithia <dieltabin@gmail.com>
2018-05-25 13:22:51 -0600

    Added matrix multiplier

--- src/clic.c
+++ src/clic.c
@@ -6,3 +6,25 @@
  * <rogers@anthro.utah.edu>. This file is released under the Internet
  * Systems Consortium License, which can be found in file "LICENSE".
  */
+
+ #include "clic.h"
+
+
+/*
+ Matrix multiplier.  Takes two matricies of floats m1 and m2 and their sizes,
+ as well as a results matrix, where the results go.
+*/
+ void mm_mult(float **m1, int m1_rows, int m1_cols,
+              float **m2, int m2_rows, int m2_cols,
+              float **results){
+   int i, j, k;
+   for (i = 0; i < m1_rows; i++){         //go through each row
+     for(j = 0; j < m2_cols; j++){        //go through each coloumn
+       int x = 0;
+       for (k = 0; k < m2_rows; k++){
+         x += (m1[i][k] * m2[k][j]);      //calculate what goes there
+       }
+       results[i][j] = x;                 //put it in results
+     }
+   }
+ }
---
+++ src/clic.h
@@ -0,0 +1,6 @@
+#ifndef LEGO_CLIC
+#define LEGO_CLIC
+
+void mm_mult(float **m1, int m1_rows, int m1_cols, float **m2, int m2_rows, int m2_cols, float **results);
+
+#endif
