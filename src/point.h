ed91ee46f564fa3c34a5832d8ea7a21118bb1365
Alan R. Rogers <rogers@anthro.utah.edu>
2018-05-27 09:54:48 -0600

    .

--- src/point.h
+++ src/point.h
@@ -5,5 +5,8 @@
 
 void Point_setNPar(int npar);
 Point *Point_new(void);
+void Point_free(Point *self);
+void Point_set(Point *self, double cost, double *par);
+double Point_get(Point *self, double *par);
 
 #endif
