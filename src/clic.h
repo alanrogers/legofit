f2bbb02a7d3c052b078bf41165a55e547ea3f405
Darokrithia <dieltabin@gmail.com>
2018-06-07 14:43:03 -0600

    More small errors

--- src/clic.h
+++ src/clic.h
@@ -9,7 +9,7 @@ char* tri_cat(char* a, char* b, char* c, char* str);
 double** get_fit_param_array(char* title, int num_files, int num_params);
 double** get_fit_param_array_num_unkown(char* title, int num_params);
 
-double*** make_covar_matrix(double** array, int files, int params);
+double** make_covar_matrix(double** array, int files, int params);
 
 double KL_to_likelihood(double KL, double* p_matrix, int p_matrix_size, double sum);
 
