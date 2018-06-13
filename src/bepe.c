37f5227ae0c2a6203efdd738acde296cb32946f7
Darokrithia <dieltabin@gmail.com>
2018-06-13 17:28:03 -0600

    Fixed command line args

--- src/bepe.c
+++ src/bepe.c
@@ -15,33 +15,46 @@ void usage(void);
 //vars
 
 const char *usageMsg =
-     "usage: bepe <file.pts> <boot1.legofit> <boot2.legofit> ...\n"
-     "  where file.pts is the .pts file produced by legofit with the real\n"
-     "  data, and each \"boot\" file is the legofit output from one bootstrap\n"
-     "  replicate. Must include .pts file and at least 2 boostrap files.\n";
+     "usage: bepe -d <real_data> -D <bootdata_1> <bootdata_2> ... -L <boot1.legofit> <boot2.legofit> ...\n"
+     "\t where real_data is the real data,\n"
+     "\t each \"bootdata_\" file is the legofit output from one bootstrap replicate,\n"
+     "\t and each \"boot#.legofit\" file is the legofit output from one bootstrap replicate\n"
+     "\t Must include real_data file and at least 2 boostrap replicates.\n";
 
-int main(){
+ void usage(void) {
+     fputs(usageMsg, stderr);
+     exit(EXIT_FAILURE);
+ }
+
+int main(int argc, char **argv){
+  int i;
   // Command line arguments specify file names
   if(argc < 4)
       usage();
 
-  const char *ptsfname = argv[1];
-  int nfiles = argc-2;  // number of bootstrap files
-  const char *bootfname[nfiles];
+  const char *realfName = argv[1];
+  int nfiles = ((argc-5)/2);  // number of bootstrap files
+  const char *legofname[nfiles];
+  const char *datafname[nfiles];
   for(i=0; i < nfiles; ++i)
-      bootfname[i] = argv[i+2];
+      datafname[i] = argv[i+4];
+  for(i=0; i < nfiles; ++i)
+      legofname[i] = argv[i+5+nfiles];
 
   // Read bootstrap files into an array of FIFO stacks
-  StrDblStack *stack[nfiles];
+  StrDblStack* data_stack[nfiles];
+  StrDblStack* lego_stack[nfiles];
+
+  StrDblStack* real_stack = parseLegofit_BEPE(realfName);
+
   for(i=0; i < nfiles; ++i) {
-      stack[i] = parseLegofit_BEPE(bootfname[i]);
-      if(i>0) {
-          if(StrDblStack_compare(stack[0], stack[i])) {
-              fprintf(stderr, "%s:%d: inconsistent parameters in"
-                      " files %s and %s\n", __FILE__,__LINE__,
-                      bootfname[0], bootfname[i]);
-              exit(EXIT_FAILURE);
-          }
+      if(StrDblStack_compare(real_stack, lego_stack[i]) ||
+         StrDblStack_compare(real_stack, data_stack[i])) {
+          fprintf(stderr, "%s:%d: inconsistent parameters in"
+                  " files\n", __FILE__,__LINE__);
+          exit(EXIT_FAILURE);
       }
+      lego_stack[i] = parseLegofit_BEPE(legofname[i]);
+      data_stack[i] = parseLegofit_BEPE(datafname[i]);
   }
 }
