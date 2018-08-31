6dff9846084787ea2090792462234364e6735645
Alan R. Rogers <rogers@anthro.utah.edu>
2018-08-31 15:42:00 -0600

    strparmap2 and its unit test are working.
    This should work just like strparmap. The only difference is that it's based
    on the code in parmap.h and parmap.src. These are generic files that implement a
    left-leaning red-black tree without specifying which variable within the structure
    is the key. That determination is made by preprocessor macro definitions within
    strparmap2.h and strparmap2.c. The goal is to have a single code base for strparmap
    and addrparmap.

--- src/parmap.h
+++ src/parmap.h
@@ -2,9 +2,11 @@
 #define LEGOFIT_STRPARMAP_H
 
 #include "typedefs.h"
+#include <stdio.h>
 
 Param      *METHOD(search)(CLASS *root, KEYTYPE key);
 CLASS      *METHOD(insert)(CLASS *root, Param *par);
 void        METHOD(free)(CLASS *h);
+void        METHOD(print)(CLASS *h, FILE *fp, int indent);
 
 #endif
