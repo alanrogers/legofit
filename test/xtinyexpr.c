d44977781e19e0bebbd78ef9aebbee20e84ed4d3
Alan R. Rogers <rogers@anthro.utah.edu>
2018-09-24 09:35:18 -0600

    add xtinyexpr.c

---
+++ test/xtinyexpr.c
@@ -0,0 +1,92 @@
+/**
+ * @file xptrset.c
+ * @author Alan R. Rogers
+ * @brief Unit test for PtrSet
+ * @copyright Copyright (c) 2018, Alan R. Rogers
+ * <rogers@anthro.utah.edu>. This file is released under the Internet
+ * Systems Consortium License, which can be found in file "LICENSE".
+ */
+
+#include "tinyexpr.h"
+#include "misc.h"
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
+#ifdef NDEBUG
+#  error "Unit tests must be compiled without -DNDEBUG flag"
+#endif
+
+int main(int argc, char **argv) {
+    int         verbose = 0;
+
+    if(argc==2 && 0==strcmp("-v", argv[1]))
+        verbose = 1;
+    else if(argc != 1) {
+        fprintf(stderr,"usage: xtinyexpr [-v]\n");
+        exit(1);
+    }
+
+    int status;
+    double w=1.0, x=2.0, y=3.0, z=4.0;
+    const int npars = 4;
+    te_variable pars[npars];
+
+    // This memset is essential, because otherwise, the type and
+    // context fields of the te_variable structures will be
+    // initialized with garbage.
+    memset(pars, 0, sizeof(pars));
+
+    pars[0].name = "w";
+    pars[0].address = &w;
+    pars[1].name = "x";
+    pars[1].address = &x;
+    pars[2].name = "y";
+    pars[2].address = &y;
+    pars[3].name = "z";
+    pars[3].address = &z;
+
+    char formula[100];
+    sprintf(formula, "%s", "w + x*y - z");
+
+    te_expr *expr = te_compile(formula, pars, npars, &status);
+    printf("status=%d\n", status);
+    if(expr == NULL) {
+        fprintf(stderr,"%s:%d: parse error\n", __FILE__,__LINE__);
+        fprintf(stderr,"  %s\n", formula);
+        fprintf(stderr,"  %*s^\nError near here\n", status-1, "");
+        exit(EXIT_FAILURE);
+    }
+
+    int len=100;
+    const double *dep[len];
+    len = te_dependencies(expr, len, dep);
+    assert(len==npars);
+    int gotw=0, gotx=0, goty=0, gotz=0;
+    for(int i=0; i<npars; ++i) {
+        if(dep[i] == &w)
+            ++gotw;
+        else if(dep[i] == &x)
+            ++gotx;
+        else if(dep[i] == &y)
+            ++goty;
+        else if(dep[i] == &z)
+            ++gotz;
+    }
+    assert(gotw==1);
+    assert(gotx==1);
+    assert(goty==1);
+    assert(gotz==1);
+
+    double val = te_eval(expr);
+    assert(val == w + x*y - z);
+
+    if(verbose)
+        te_print(expr, stdout);
+    
+    te_free(expr);
+
+    unitTstResult("tinyexpr", "OK");
+    return 0;
+}
+    
