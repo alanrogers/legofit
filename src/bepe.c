a72d6521b63b04fee1cb1eae464d3bc98cd0c29a
Alan R. Rogers <rogers@anthro.utah.edu>
2019-02-08 20:17:38 -0700

    Update documentation of bepe.c

--- src/bepe.c
+++ src/bepe.c
@@ -51,9 +51,10 @@ differences in locale settings on different machines. The example
 command in the preceding paragraph may generate file names in
 different orders on different machines. To ensure a consistent order
 on Unix-like operating systems (linux and osx), set the LC_ALL
-environment parameter as part of the `bepe` command:
+environment parameter first. Using the bash shell:
 
-    LC_ALL=C bepe realdat.txt boot*.txt -L real.legofit boot*.legofit
+    export LC_ALL=C
+    bepe realdat.txt boot*.txt -L real.legofit boot*.legofit
 
 This ensures that file names will be listed in the same order,
 regardless of the locale setting of the local machine.
@@ -62,82 +63,30 @@ The first few lines of `bepe`'s output look like this:
 
     ################################################
     # bepe: bootstrap estimate of predictive error #
-    #                 version 1.47                 #
+    #                 version 1.80                 #
     ################################################
 
-    # Program was compiled: Jul 29 2018 08:45:55
-    # Program was run: Sun Jul 29 08:53:47 2018
+    # Program was compiled: Feb  8 2019 15:26:39
+    # Program was run: Fri Feb  8 15:31:50 2019
+    # Not correcting for bootstrap bias
 
-    #          bepe DataFile
-    3.599051951e-07 mpqva1.txt
-    3.937402126e-07 mpqvasing000
-     3.65923644e-07 mpqvasing001
-    4.205461171e-07 mpqvasing002
-    3.841736741e-07 mpqvasing003
-    4.103783016e-07 mpqvasing004
-    4.357691253e-07 mpqvasing005
-    3.996038295e-07 mpqvasing006
+    #          bepe        DataFile     LegofitFile
+    1.908861378e-07        sim0.opf    a2-0.legofit
+    2.495970126e-07        sim1.opf    a2-1.legofit
+    2.627267214e-07       sim10.opf   a2-10.legofit
+     1.71297571e-07       sim11.opf   a2-11.legofit
+    1.917256551e-07       sim12.opf   a2-12.legofit
 
 Many of the lines begin with the comment character (#) so that they
 can be removed easily in downstream processing. Each uncommented line
-refers to a particular data set, whose name is given in column
-2. Typically, the first file is the real data, and the others are
+refers to a particular data sets, whose names are given in columns
+2-3. Typically, the first file is the real data, and the others are
 bootstrap replicates. Column 1 (bepe) gives the mean squared
 difference between the site pattern frequencies in this data set, and
-the predictions obtained from all the other data sets. (Bepe also
+the predictions obtained from all the other data sets. Bepe also
 includes a bias correction, which is described by Efron and
-Tibshirani.) The predicted values are taken from the various
+Tibshirani. The predicted values are taken from the various
 `.legofit` files.
-
-The next section lists the two sets of input files. Here are a few
-illustrative lines:
-
-    #  i         datfile[i]        legofile[i]
-    #  0         mpqva1.txt         t3.legofit
-    #  1       mpqvasing000    t3boot0.legofit
-    #  2       mpqvasing001    t3boot1.legofit
-    #  3       mpqvasing002    t3boot2.legofit
-    #  4       mpqvasing003    t3boot3.legofit
-    #  5       mpqvasing004    t3boot4.legofit
-
-The first column gives the index of the current data set, the second
-names the data file containing site pattern frequencie, and the third
-names the `legofit` output generated from that data file. The
-`legofit` output includes predicted site pattern frequencies, which
-are used by `bepe`.
-
-This section of output is provided to help find a particular kind of
-mistake. Suppose we had run `bepe` like this:
-
-    bepe mpqva1.txt mpqvasing* -L t3.legofit t3boot*.legofit
-
-This generates erroneous output, which can be detected by examining
-the 2nd portion of `bepe`'s output. Here are the first few lines:
-
-    #  i         datfile[i]        legofile[i]
-    #  0         mpqva1.txt         t3.legofit
-    #  1       mpqvasing000    t3boot0.legofit
-    #  2       mpqvasing001   t3boot10.legofit
-
-Note that bootstrap data file `mpqvasing001` is paired with `legofit`
-file `t3boot10.legofit`. This is a mistake. This data file should have
-been paired with `t3boot1.legofit`. The problem is that the wildcards
-characters (\*) expand to file names sorted in lexical order. Because
-of the leading zeroes in the data file names, a lexical sort
-puts these names into into numerical order. But there are no leading
-zeroes in the `.legofit` filenames, so they sort in a different
-order. This generates a mismatch between the data files and the
-`.legofit` files.
-
-To avoid such problems, you need to generate both sets of file names
-in a consistent order. Here's a `bash` command line that solves the
-above problem:
-
-    bepe mpqva1.txt mpqvasing* -L t3.legofit `seq 0 49 | \
-    xargs printf "t3boot%d.legofit "`
-
-The last section of `bepe`'s output echoes the command line, which
-serves as documentation.
 
 @copyright Copyright (c) 2018, Alan R. Rogers
 <rogers@anthro.utah.edu>. This file is released under the Internet
@@ -283,7 +232,7 @@ int main(int argc, char **argv) {
                 bias += StrDblQueue_msd(data_queue[j], lego_queue[j]);
         }
         bepe = (msd+bias)/(nfiles-1);
-        printf("%15.10lg %15s %15s\n", bepe, 
+        printf("%15.10lg %15s %15s\n", bepe,
                mybasename(datafname[i]),
                mybasename(legofname[i]));
     }
