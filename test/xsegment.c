7d5360e17b08fd056c769faf8422554ae9a71359
Alan R. Rogers <rogers@anthro.utah.edu>
2020-07-21 12:31:58 -0600

    .

--- test/xsegment.c
+++ test/xsegment.c
@@ -20,7 +20,7 @@
 
 int main(int argc, char *argv[]) {
 
-    int verbose = 0;
+    int status, verbose = 0;
 
     switch (argc) {
     case 1:
@@ -34,99 +34,156 @@ int main(int argc, char *argv[]) {
         goto usage;
     }
 
-    int         i, status, ok=1;
-    const int   nseg = 7;
-    Segment    *s[nseg];
-    double      twoN=100.0;
-    double      t0=0.0, t1=1.0, t2=2.0, t3=3.0;
-    double      mix = 0.5;
 
+    PtrQueue *fixedQ = PtrQueue_new();
+    PtrQueue *freeQ = PtrQueue_new();
+    PtrQueue *constrQ = PtrQueue_new();
 
-    s[0] = Segment_new(&twoN, &t0, 0);
-    s[1] = Segment_new(&twoN, &t0, 0);
-    s[2] = Segment_new(&twoN, &t0, 0);
-    s[3] = Segment_new(&twoN, &t1, 0);
-    s[4] = Segment_new(&twoN, &t1, 0);
-    s[5] = Segment_new(&twoN, &t2, 0);
-    s[6] = Segment_new(&twoN, &t3, 0);
+    Param *par;
 
-    for(i=0; i<nseg; ++i) {
-        assert(s[i]->twoN == &twoN);
-        assert(s[i]->end == NULL);
-        assert(s[i]->mix == NULL);
-        assert(s[i]->nsamples == 0);
-        assert(s[i]->nchildren == 0);
-        assert(s[i]->child[0] == NULL);
-        assert(s[i]->child[1] == NULL);
-        assert(s[i]->parent[0] == NULL);
-        assert(s[i]->parent[1] == NULL);
+    par = Param_new("zero", 0.0, 0.0, 0.0, TIME|FIXED, NULL);
+    PtrQueue_push(fixedQ, par);
+
+    par = Param_new("one", 1.0, 1.0, 1.0, TWON|FIXED, NULL);
+    PtrQueue_push(fixedQ, par);
+
+    par = Param_new("Nab", 3.0, 0.0, 100.0, TWON|FREE, NULL);
+    PtrQueue_push(freeQ, par);
+
+    par = Param_new("Tab", 2.0, 0.0, 100.0, TIME|FREE, NULL);
+    PtrQueue_push(freeQ, par);
+
+    par = Param_new("Tmig", 1.0, 1.0, 1.0, TIME|FIXED, NULL);
+    PtrQueue_push(fixedQ, par);
+
+    par = Param_new("mix", 0.02, 0.02, 0.02, MIXFRAC|FIXED, NULL);
+    PtrQueue_push(fixedQ, par);
+
+    par = Param_new("Nabc", 3.0, 0.0, 100.0, TWON|FREE, NULL);
+    PtrQueue_push(freeQ, par);
+
+    par = Param_new("Tabc", 4.0, -DBL_MAX, DBL_MAX, TIME|CONSTRAINED,
+                    "Tab + Nab*Nabc");
+    PtrQueue_push(constrQ, par);
+
+    ParStore *ps = ParStore_new(fixedQ, freeQ, constrQ);
+
+    if(verbose)
+        ParStore_print(ps, stderr);
+
+    Bounds bnd = {
+        .lo_twoN = 0.0,
+        .hi_twoN = 1e12,
+        .lo_t = 0,
+        .hi_t = 1e10
+    };
+
+    Segment *a, *b, *b2, *c, *c2, *ab, *abc;
+    int ni, ti, mi;
+    tipId_t     ida = 0;
+    tipId_t     idb = 1;
+    tipId_t     idc = 2;
+    Gene       *ga = Gene_new(ida);
+    Gene       *gb = Gene_new(idb);
+    Gene       *gc = Gene_new(idc);
+
+    ni = ParStore_getIndex(ps, "one");
+    assert(ni >= 0);
+    ti = ParStore_getIndex(ps, "zero");
+    assert(ti >= 0);
+
+    a = Segment_new(ni, ti, ps);
+    assert(a);
+
+    b = Segment_new(ni, ti, ps);
+    assert(b);
+
+    c = Segment_new(ni, ti, ps);
+    assert(c);
+    
+    ti = ParStore_getIndex(ps, "Tmig");
+    assert(ti >= 0);
+    b2 = Segment_new(ni, ti, ps);
+    c2 = Segment_new(ni, ti, ps);
+    assert(b2);
+    assert(c2);
+
+    ni = ParStore_getIndex(ps, "Nab");
+    assert(ni >= 0);
+    ti = ParStore_getIndex(ps, "Tab");
+    assert(ti >= 0);
+    ab = Segment_new(ni, ti, ps);
+    assert(ab);
+
+    ni = ParStore_getIndex(ps, "Nabc");
+    assert(ni >= 0);
+    ti = ParStore_getIndex(ps, "Tabc");
+    assert(ti >= 0);
+    abc = Segment_new(ni, ti, ps);
+    assert(abc);
+    
+    status = Segment_addChild(ab, a);
+    assert(status == 0);
+
+    mi = ParStore_getIndex(ps, "mix");
+    assert(mi >= 0);
+    status = Segment_mix(b, mi, c2, b2, ps);
+
+    status = Segment_addChild(c2, c);
+    assert(status == 0);
+
+    status = Segment_addChild(ab, b2);
+    assert(status == 0);
+    
+    status = Segment_addChild(abc, ab);
+    assert(status == 0);
+
+    status = Segment_addChild(abc, c2);
+    assert(status == 0);
+
+    if(verbose) {
+        Segment_printShallow(abc, stdout);
+        Segment_printShallow(ab, stdout);
+        Segment_printShallow(a, stdout);
+        Segment_printShallow(b, stdout);
+        Segment_printShallow(c, stdout);
     }
 
-    char buff[100];
+    assert(Segment_isClear(abc));
 
-    /*
-      2-4----+
-        |    |
-        V    6-
-      1-3-+  |
-          5--+
-      0---+
-     */
+    Segment_transferSample(a, ga);
+    Segment_transferSample(b, gb);
+    Segment_transferSample(c, gc);
 
-    // 1 is a mixture of 3 and 4
-    status = Segment_mix(s[1], &mix, s[4], s[3]);
-    if(status) {
-        mystrerror_r(status, buff, sizeof(buff));
-        fprintf(stderr,"%s:%d: %s\n", __FILE__,__LINE__,buff);
-        ok=0;
-    }
+    assert(!Segment_isClear(abc));
 
-    status = Segment_addChild(s[5], s[0]); // derive 0 from 5
-    if(status) {
-        mystrerror_r(status, buff, sizeof(buff));
-        fprintf(stderr,"%s:%d: %s\n", __FILE__,__LINE__,buff);
-        ok=0;
-    }
+    Segment_unvisit(abc);
+    Gene *root = Segment_coalesce(abc, rng);
+    assert(root != NULL);
 
-    status = Segment_addChild(s[5], s[3]); // derive 3 from 5
-    if(status) {
-        mystrerror_r(status, buff, sizeof(buff));
-        fprintf(stderr,"%s:%d: %s\n", __FILE__,__LINE__,buff);
-        ok=0;
-    }
+    assert(!Segment_isClear(abc));
+    Segment_clear(abc);
+    assert(Segment_isClear(abc));
 
-    status = Segment_addChild(s[4], s[2]); // derive 2 from 4
-    if(status) {
-        mystrerror_r(status, buff, sizeof(buff));
-        fprintf(stderr,"%s:%d: %s\n", __FILE__,__LINE__,buff);
-        ok=0;
-    }
+    assert(abc == Segment_root(a));
+    assert(abc == Segment_root(b));
+    assert(abc == Segment_root(b2));
+    assert(abc == Segment_root(c));
+    assert(abc == Segment_root(c2));
+    assert(abc == Segment_root(ab));
+    assert(abc == Segment_root(abc));
 
-    status = Segment_addChild(s[6], s[4]); // derive 4 from 6
-    if(status) {
-        mystrerror_r(status, buff, sizeof(buff));
-        fprintf(stderr,"%s:%d: %s\n", __FILE__,__LINE__,buff);
-        ok=0;
-    }
+    Segment_clear(abc);
+    assert(Segment_feasible(abc, bnd, verbose));
 
-    status = Segment_addChild(s[6], s[5]); // derive 5 from 6
-    if(status) {
-        mystrerror_r(status, buff, sizeof(buff));
-        fprintf(stderr,"%s:%d: %s\n", __FILE__,__LINE__,buff);
-        ok=0;
-    }
+    PtrPtrMap *ppm = PtrPtrMap_new();
+    Segment *duproot = Segment_dup(abc, ppm);
+    CHECKMEM(duproot);
+    assert(Segment_feasible(duproot, bnd, verbose));
+    Gene_free(root);
+    PtrPtrMap_free(ppm);
 
-    Segment *root = Segment_root(s[0]);
-    for(i=1; i<nseg; ++i) {
-        Segment *r = Segment_root(s[i]);
-        if(r != root) {
-            fprintf(stderr,"%s:%d segments 0 and %d have different roots\n",
-                    __FILE__,__LINE__, i);
-            ok=0;
-        }
-    }
-
-    Segment_sanityCheck(root, __FILE__, __LINE__);
+    assert(Segment_equals(abc, duproot));
 
     if(verbose)
         Segment_print(stdout, root, 0);
