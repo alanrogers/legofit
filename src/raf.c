cfd776a1c694ea16aaafb08f5f90d03d82f3021f
Alan R. Rogers <rogers@anthro.utah.edu>
2026-06-26 11:17:45 -0600

    Modified raf.c: added tests for empty input fields, which now cause the program to abort with an error message.

--- src/raf.c
+++ src/raf.c
@@ -93,11 +93,41 @@ int main(int argc, char **argv) {
         }
         char       *chr, *pos, *reftoken, *alttoken;
         char       *gtype, *next = buff;
+        int         empty_field = 0;
 
         chr = strsep(&next, "\t");  // field 0
+        if(chr==NULL) {
+            fprintf(stderr, "%s:%d: empty chr field in input\n",
+                    __FILE__, __LINE__);
+            empty_field=1;
+        }
+
         pos = strsep(&next, "\t");  // field 1
+        if(pos==NULL) {
+            fprintf(stderr, "%s:%d: empty pos field in input\n",
+                    __FILE__, __LINE__);
+            empty_field=1;
+        }
+
         reftoken = strsep(&next, "\t");  // field 2
+        if(reftoken==NULL) {
+            fprintf(stderr, "%s:%d: empty ref field in input\n",
+                    __FILE__, __LINE__);
+            empty_field=1;
+        }
+
         alttoken = strsep(&next, "\t");  // field 3
+        if(alttoken==NULL) {
+            fprintf(stderr, "%s:%d: empty alt field in input\n",
+                    __FILE__, __LINE__);
+            empty_field=1;
+        }
+
+        if(empty_field) {
+            fprintf(stderr, "%s:%d: aborting; empty input field(s)\n",
+                    __FILE__, __LINE__);
+            exit(EXIT_FAILURE);
+        }
 
         nucpos = strtoul(pos, NULL, 10);
 
